30822f3d9ea742518ea1f47ad0a278d40ad3a725
lucyyxc <xilucychen@gmail.com>
2020-10-28 21:05:29 -0700

    schema planned

--- server/schema.sql
+++ server/schema.sql
@@ -1,11 +1,49 @@
 CREATE DATABASE chat;
-
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- //specifies DATABASE
+CREATE TABLE Messages (
+  MessagesID INT(11) not null primary key auto_increment,
+  text VARCHAR(280),
+  User INT(11),
+  FOREIGN KEY (User) REFERENCES User(UserID),
+  Room INT(11)
+  FOREIGN KEY (Room) REFERENCES Room(RoomID),
 );
 
+
+CREATE TABLE User (
+  UserID INT(11) not null primary key auto_increment,
+  Name VARCHAR(280)
+);
+
+
+CREATE TABLE Room (
+  RoomID INT(11) not null primary key auto_increment,
+  Name VARCHAR(280)
+);
+
+CREATE TABLE UserRoom (
+  UserRoomID INT(11) not null primary key auto_increment,
+  User INT(11),
+  FOREIGN KEY (User) REFERENCES User(UserID),
+  RoomID INT(11),
+  FOREIGN KEY (Room) REFERENCES Room(RoomID)
+);
+
+
+INSERT INTO User VALUES ('null', 'Jacky');
+INSERT INTO User VALUES ('null', 'Lucy');
+INSERT INTO User VALUES ('null', 'Chris');
+INSERT INTO User VALUES ('null', 'Tony');
+
+INSERT INTO Room VALUES ('null', 'Lobby');
+INSERT INTO Room VALUES ('null', 'PentHouse');
+INSERT INTO Room VALUES ('null', 'School');
+INSERT INTO Room VALUES ('null', 'Kitchen');
+
+
+
+INSERT INTO Messages VALUES ('null', 'Lobby');
 /* Create other tables and define schemas for them here! */
 
 
@@ -15,3 +53,15 @@ CREATE TABLE messages (
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- SHOW TABLES; //all tables in database
+
+-- CREATE TABLE pet (primarykey INT(11), name VARCHAR(20), owner VARCHAR(20),
+--    );
+
+-- DESCRIBE pet //to see how pet table looks like
+
+-- SELECT * FROM pet //to see table
+
+-- INSERT INTO pet VALUES ('2', 'Walter', 'Lucy');
+
+-- UPDATE pet SET owner //key = ‘timothy’ //new value WHERE name=‘mochi’;
